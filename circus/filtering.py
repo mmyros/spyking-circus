ac2821e6937fd04403c50d83faf2ae1ffd6c2bbf
Max <mmyros@gmail.com>
2017-04-03 19:33:27 -0400

    Wavelet filtering

--- circus/filtering.py
+++ circus/filtering.py
@@ -50,7 +50,7 @@ def main(params, nb_cpu, nb_gpu, use_gpu):
         chunk_size    = params.getint('data', 'chunk_size')
         nb_chunks, _  = data_file_in.analyze(chunk_size)
 
-        b, a          = signal.butter(3, np.array(cut_off)/(params.rate/2.), 'pass')
+        #b, a          = signal.butter(3, np.array(cut_off)/(params.rate/2.), 'pass')
         all_chunks    = numpy.arange(nb_chunks, dtype=numpy.int64)
         to_process    = all_chunks[comm.rank::comm.size]
         loc_nb_chunks = len(to_process)
@@ -75,12 +75,13 @@ def main(params, nb_cpu, nb_gpu, use_gpu):
             local_chunk, t_offset =  data_file_in.get_data(gidx, chunk_size)
 
             if do_filtering:
-                for i in nodes:    
-                    try:           
-                        local_chunk[:, i]  = signal.filtfilt(b, a, local_chunk[:, i])
+                for i in nodes:
+                    try:
+                        #local_chunk[:, i]  = signal.filtfilt(b, a, local_chunk[:, i])
+                        local_chunk[:, i]  = WMLDR(local_chunk[:, i])
                     except Exception:
                         pass
-                local_chunk[:, i] -= numpy.median(local_chunk[:, i]) 
+                local_chunk[:, i] -= numpy.median(local_chunk[:, i])
 
             if do_remove_median:
                 if not numpy.all(nodes == numpy.arange(N_total)):
@@ -104,6 +105,69 @@ def main(params, nb_cpu, nb_gpu, use_gpu):
 
         comm.Barrier()
 
+    def WMLDR(data, wname="db4", maxlevel=6, mode='sym'):
+        """Perform wavelet multi-level decomposition and reconstruction (WMLDR) on multichannel
+        data. See Wiltschko2008. Default to Daubechies(4) wavelet. Modifies data in-place, at
+        least for now. The effective cutoff frequency is:
+
+        fc = (sampfreq / 2) / 2**maxlevel                     (Wiltschko2008)
+
+        For sampfreq of 25 kHz and maxlevel of 6, the effective cutoff frequency is 195 Hz.
+        For sampfreq of 30 kHz and maxlevel of 6, the effective cutoff frequency is 234 Hz.
+
+        TODO: for now, this only returns highpass data. In the future, this probably should
+        return both low and highpass data (and not modify it in-place). The Discussion in
+        Wiltschko2008 suggests that this approach cannot be used to extract the LFP, but
+        I don't see why you can't simply subtract the highpass data from the raw data to get the
+        lowpass data.
+
+        Signal extension modes (from PyWavelets docs):
+
+        PyWavelets provides several methods of signal extrapolation that can be used to minimize
+        edge effects. PyWavelet's default is 'sym':
+
+        zpd - zero-padding - signal is extended by adding zero samples:
+        ... 0  0 | x1 x2 ... xn | 0  0 ...
+
+        cpd - constant-padding - border values are replicated:
+        ... x1 x1 | x1 x2 ... xn | xn xn ...
+
+        sym - symmetric-padding - signal is extended by mirroring samples:
+        ... x2 x1 | x1 x2 ... xn | xn xn-1 ...
+
+        ppd - periodic-padding - signal is treated as a periodic one:
+        ... xn-1 xn | x1 x2 ... xn | x1 x2 ...
+
+        sp1 - smooth-padding - signal is extended according to the first derivatives calculated on
+        the edges (straight line)
+
+        DWT performed for these extension modes is slightly redundant, but ensures perfect
+        reconstruction. To receive the smallest possible number of coefficients, computations can
+        be performed with the periodization mode:
+
+        per - periodization - is like periodic-padding but gives the smallest possible number of
+        decomposition coefficients. IDWT must be performed with the same mode.
+        """
+        import pywt
+
+        data = np.atleast_2d(data)
+        nt = data.shape[1]
+        # reconstructed signals always seem to have an even number of points. If the number of
+        # input data points is odd, trim last data point from reconstructed signal:
+        isodd = nt % 2
+        # filter data in place, iterate over channels in rows:
+        nchans = len(data)
+        for chani in range(nchans):
+            # decompose the signal:
+            cs = pywt.wavedec(data[chani], wname, mode=mode, level=maxlevel)
+            # destroy the appropriate approximation coefficients to get highpass data:
+            cs[0] = None
+            # reconstruct the signal:
+            recsignal = pywt.waverec(cs, wname, mode=mode)
+            ntrec = len(recsignal)
+            data[chani] = recsignal[:ntrec-isodd]
+
+        return data
 
     def compute_artefacts(data_file):
 
@@ -197,7 +261,7 @@ def main(params, nb_cpu, nb_gpu, use_gpu):
                 print_and_log(['Artefact times are read in timesteps'], 'debug', logger)
             artefacts        = artefacts.astype(numpy.int64)
             windows          = windows.astype(numpy.int64)
- 
+
         nb_stimuli       = len(numpy.unique(artefacts[:, 0]))
         mytest           = nb_stimuli == len(windows)
 
@@ -278,7 +342,7 @@ def main(params, nb_cpu, nb_gpu, use_gpu):
         data_file_in._params = tmp_params
         if data_file_in.is_stream:
             for source in data_file_in._sources:
-                source._params = tmp_params   
+                source._params = tmp_params
 
     if clean_artefact:
         if not (os.path.exists(params.get('triggers', 'trig_file')) and os.path.exists(params.get('triggers', 'trig_windows'))):
